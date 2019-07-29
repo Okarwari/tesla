920dd712fcf3de37c012da2721d640d280166014
Goook <chengzhengwu@aliyun.com>
2019-07-29 12:55:11 +0800

    fix'

--- debug.py
+++ debug.py
@@ -1,11 +1,38 @@
-from memory_profiler import profile
+import asyncio
+import random
+import time
 
-@profile
-def chg_nums(n):
-    n = 20
-def chg_list(l):
-    l.append(20)
-a = 18
-chg_nums(a)
-b = [1,2]
-chg_list(b)
+
+async def consumer(queue, id):
+    while True:
+        val = await queue.get()
+        print('{} get a val: {}'.format(id, val))
+        await asyncio.sleep(1)
+
+
+async def producer(queue, id):
+    for i in range(5):
+        val = random.randint(1, 10)
+        await queue.put(val)
+        print('{} put a val: {}'.format(id, val))
+        await asyncio.sleep(1)
+
+
+async def main():
+    queue = asyncio.Queue()
+
+    consumer_1 = asyncio.create_task(consumer(queue, 'consumer_1'))
+    consumer_2 = asyncio.create_task(consumer(queue, 'consumer_2'))
+
+    producer_1 = asyncio.create_task(producer(queue, 'producer_1'))
+    producer_2 = asyncio.create_task(producer(queue, 'producer_2'))
+
+    await asyncio.sleep(10)
+    consumer_1.cancel()
+    consumer_2.cancel()
+
+    await asyncio.gather(consumer_1, consumer_2, producer_1, producer_2, return_exceptions=True)
+import time
+t = time.time()
+asyncio.run(main())
+print(time.time() - t)
